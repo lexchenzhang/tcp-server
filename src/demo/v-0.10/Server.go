ba20271ccf05fd77d5bcb9f2ea25cc0a77e8ca8f
Lex <lexzh@outlook.com>
2023-03-09 18:13:40 -0600

    add conn property

---
+++ src/demo/v-0.10/Server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"tcp-server/src/lex/ziface"
+	"tcp-server/src/lex/znet"
+)
+
+// ping test - customized router
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+// hi test - customized router
+type HiRouter struct {
+	znet.BaseRouter
+}
+
+// ping router
+func (pr *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("call router handle...")
+	fmt.Println("recv from client: msgID=", request.GetMsgID(), " data=", string(request.GetData()))
+	err := request.GetConnection().SendMsg(0, []byte("ping..."))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// hello router
+func (hr *HiRouter) Handle(request ziface.IRequest) {
+	err := request.GetConnection().SendMsg(1, []byte("hi..."))
+	fmt.Println("recv from client: msgID=", request.GetMsgID(), " data=", string(request.GetData()))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// onstart hooker
+func DoConnectionBegin(conn ziface.IConnection) {
+	fmt.Println("user's on conn start....")
+	err := conn.SendMsg(202, []byte("special pkg on start"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn.SetProperty("Str", "test_str")
+	conn.SetProperty("Int", 512)
+}
+
+func DoConnectionClose(conn ziface.IConnection) {
+	fmt.Println("user's on conn stop....")
+	v_str, _ := conn.GetProperty("Str")
+	v_int, _ := conn.GetProperty("Int")
+	fmt.Println(v_str, " ", v_int)
+}
+
+func main() {
+	s := znet.NewServer("[server v0.10]")
+	// register hook func
+	s.SetOnConnStart(DoConnectionBegin)
+	s.SetOnConnStop(DoConnectionClose)
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HiRouter{})
+	s.Serve()
+}
--- src/lex/ziface/iconnection.go
+++ src/lex/ziface/iconnection.go
@@ -11,6 +11,9 @@ type IConnection interface {
 	GetConnID() uint32
 	RemoteAddr() net.Addr
 	SendMsg(msgId uint32, data []byte) error
+	SetProperty(key string, value interface{})
+	GetProperty(key string) (interface{}, error)
+	RemoveProperty(key string)
 }
 
 type HandleFunc func(*net.TCPConn, []byte, int) error
