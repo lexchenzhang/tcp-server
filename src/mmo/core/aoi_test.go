43d27e87ca889b838b894e6b1263b38a123d5614
Lex <lexzh@outlook.com>
2023-03-10 20:58:44 -0600

    add AOI related

--- src/mmo/core/aoi_test.go
+++ src/mmo/core/aoi_test.go
@@ -2,10 +2,33 @@ package core
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestNewAOIManager(t *testing.T) {
 	aoiMgr := NewAOIManager(0, 250, 5, 0, 250, 5)
 	fmt.Println(aoiMgr)
+}
+
+func TestAOIManagerSuroundGrIDsByGID(t *testing.T) {
+	aoiMgr := NewAOIManager(0, 250, 5, 0, 250, 5)
+	var tests = []struct {
+		name     string
+		gridID   int
+		expected []int
+	}{
+		{"case 1", 0, []int{1, 5, 6}},
+		{"case 2", 4, []int{3, 8, 9}},
+		{"case 3", 11, []int{5, 6, 7, 10, 12, 15, 16, 17}},
+		{"case 4", 22, []int{16, 17, 18, 21, 23}},
+	}
+
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			if _, ret := aoiMgr.GetSurroundGrIDsByGID(c.gridID); !reflect.DeepEqual(ret, c.expected) {
+				t.Errorf("should be %v but got %v", c.expected, ret)
+			}
+		})
+	}
 }
--- src/mmo/core/grid.go
+++ src/mmo/core/grid.go
@@ -5,18 +5,22 @@ import (
 	"sync"
 )
 
-type Grid struct {
-	GID       int
-	MinX      int
-	MaxX      int
-	MinY      int
-	MaxY      int
-	playerIDs map[int]bool
-	pIDLock   sync.RWMutex
+/*
+	一个地图中的格子类
+*/
+type GrID struct {
+	GID       int          //格子ID
+	MinX      int          //格子左边界坐标
+	MaxX      int          //格子右边界坐标
+	MinY      int          //格子上边界坐标
+	MaxY      int          //格子下边界坐标
+	playerIDs map[int]bool //当前格子内的玩家或者物体成员ID
+	pIDLock   sync.RWMutex //playerIDs的保护map的锁
 }
 
-func NewGrid(gID, minX, maxX, minY, maxY int) *Grid {
-	return &Grid{
+//初始化一个格子
+func NewGrID(gID, minX, maxX, minY, maxY int) *GrID {
+	return &GrID{
 		GID:       gID,
 		MinX:      minX,
 		MaxX:      maxX,
@@ -26,27 +30,35 @@ func NewGrid(gID, minX, maxX, minY, maxY int) *Grid {
 	}
 }
 
-func (g *Grid) Add(playerID int) {
+//向当前格子中添加一个玩家
+func (g *GrID) Add(playerID int) {
 	g.pIDLock.Lock()
 	defer g.pIDLock.Unlock()
+
 	g.playerIDs[playerID] = true
 }
 
-func (g *Grid) Remove(playerID int) {
+//从格子中删除一个玩家
+func (g *GrID) Remove(playerID int) {
 	g.pIDLock.Lock()
 	defer g.pIDLock.Unlock()
+
 	delete(g.playerIDs, playerID)
 }
 
-func (g *Grid) GetPlayerIDs() (playerIDs []int) {
+//得到当前格子中所有的玩家
+func (g *GrID) GetPlyerIDs() (playerIDs []int) {
 	g.pIDLock.RLock()
 	defer g.pIDLock.RUnlock()
-	for k := range g.playerIDs {
+
+	for k, _ := range g.playerIDs {
 		playerIDs = append(playerIDs, k)
 	}
-	return playerIDs
+
+	return
 }
 
-func (g *Grid) String() string {
-	return fmt.Sprintf("Grid id: %d, minX:%d, maxX:%d, minY:%d, maxY:%d, playerIDs:%v", g.GID, g.MinX, g.MaxX, g.MinY, g.MaxY, g.playerIDs)
+func (g *GrID) String() string {
+	return fmt.Sprintf("GrID ID: %d, minX:%d, maxX:%d, minY:%d, maxY:%d, playerIDs:%v",
+		g.GID, g.MinX, g.MaxX, g.MinY, g.MaxY, g.playerIDs)
 }
